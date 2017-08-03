2c5c89b23647bd81ac9293e4cd4d945a0f53f984
aaron.corley <aaron.corley@sirsidynix.com>
2017-08-02 22:23:19 -0600

    added paths

--- pubg-bravery.js
+++ pubg-bravery.js
@@ -2,6 +2,13 @@ var MAP_WIDTH_PX = 5184;
 var MAP_WIDTH_METERS = 8000;
 var PPM = MAP_WIDTH_PX / MAP_WIDTH_METERS;
 
+var body = document.body,
+    html = document.documentElement;
+
+var height = Math.max( body.scrollHeight, body.offsetHeight, 
+                       html.clientHeight, html.scrollHeight, html.offsetHeight );
+document.getElementById('map-erangel').style = "height: " + height + "px";
+
 var map = L.map('map-erangel', {
     crs: L.CRS.Simple,
     attributionControl: false,
@@ -36,7 +43,6 @@ for (var i = 1; i < 8; i++) {
 
 // click-drag
 var startCircle = null;
-var endCircle = null;
 var line = null;
 var dragging = false;
 
@@ -46,9 +52,6 @@ map.on("mousedown", function(event) {
     if (startCircle) {
         startCircle.remove();
     }
-    if (endCircle) {
-        endCircle.remove();
-    }
     if (line) {
         line.remove();
     }
@@ -56,8 +59,62 @@ map.on("mousedown", function(event) {
     startCircle.addTo(map);
 });
 map.on("mouseup", function(event) {
-    endCircle = L.circle(event.latlng, { radius: 100, color: "white" });
-    endCircle.addTo(map);
+    var endLat = event.latlng.lat;
+    var endLng = event.latlng.lng;
+    var startLat = startCircle.getLatLng().lat;
+    var startLng = startCircle.getLatLng().lng;
+
+    var slope = (endLat - startLat) / (endLng - startLng);
+    var b = endLat - (slope * endLng);
+
+    var leftIntercept = b;
+    var rightIntercept = MAP_WIDTH_PX * slope + b;
+
+    var degToRad = function(deg) {
+        return deg * Math.PI / 180;
+    };
+    var radToDeg = function(rad) {
+        return rad * 180 / Math.PI;
+    };
+
+    var angle = radToDeg(Math.atan2(slope, 1));
+    var LONG_RANGE = 2200 * PPM;
+    var MED_RANGE = 1300 * PPM;
+
+    var angleX = angle;
+    var angleY = 90;
+    var angleZ = 180 - angleX - angleY;
+
+    var yLongRangeOffset = LONG_RANGE * Math.sin(degToRad(90)) / Math.sin(degToRad(angleZ));
+    var yMedRangeOffset = MED_RANGE * Math.sin(degToRad(90)) / Math.sin(degToRad(angleZ));
+
+    // TODO: conform to map bounds
+    var buildPathPolygons = function(yOffset, color) {
+        L.polygon(
+            [
+                [ [leftIntercept, 0], [leftIntercept + yOffset, 0], [rightIntercept + yOffset, MAP_WIDTH_PX], [rightIntercept, MAP_WIDTH_PX], [leftIntercept, 0] ]
+            ],
+            { color: color }
+        ).addTo(map);
+        L.polygon(
+            [
+                [ [leftIntercept, 0], [leftIntercept - yOffset, 0], [rightIntercept - yOffset, MAP_WIDTH_PX], [rightIntercept, MAP_WIDTH_PX], [leftIntercept, 0] ]
+            ],
+            { color: color }
+        ).addTo(map);
+    }
+
+    buildPathPolygons(yLongRangeOffset, "blue");
+    buildPathPolygons(yMedRangeOffset, "orange");
+    L.polyline([[leftIntercept, 0], [rightIntercept, MAP_WIDTH_PX]], { color: "white" }).addTo(map);
+
+    if (startCircle) {
+        startCircle.remove();
+    }
+    if (line) {
+        line.remove();
+    }
+
     map.dragging.enable();
     dragging = false;
 });
